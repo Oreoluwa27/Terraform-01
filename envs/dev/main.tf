f2c079c1188d345a8302228019dfefbc9f525661
Oreoluwa Akintomowo <akintomowooreoluwa@gmail.com>
2025-07-11 17:00:07 +0100

    db access

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -58,4 +58,5 @@ resource "aws_db_instance" "default" {
   password             = "Adminpassword2004!"
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
+  publicly_accessible = true
 }
