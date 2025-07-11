f67dcf7d7d50f4303e024ca3eca10ff21eba4af7
Oreoluwa Akintomowo <akintomowooreoluwa@gmail.com>
2025-07-11 10:17:48 +0100

    overwritten code corrected

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -3,20 +3,9 @@ variable "environment" {
   type        = string
 }
 
-variable "vpc_id" {
-  description = "The ID of the VPC where the compute resources will be deployed."
-  type        = string
-
-}
-
 variable "cluster_name" {
   description = "The name of the environment cluster"
   type        = string
-}
-
-variable "subnet_ids" {
-  description = "A list of subnet IDs to deploy instances into."
-  type        = list(string)
 }
 
 variable "kubernetes_version" {
@@ -24,20 +13,10 @@ variable "kubernetes_version" {
   type        = string
 }
 
-variable "security_group_ids" {
-  description = "A list of security group IDs to attach to the instances."
-  type        = list(string)
-}
 
 variable "admin_iam_role_arns" {
   description = "A list of IAM Role ARNs that should be granted cluster-admin access via EKS Access Entries. Max 1 for simplicity in this example."
   type        = list(string)
-}
-
-variable "default_tags" {
-  description = "A map of default tags to apply to all resources created by this module."
-  type        = map(string)
-  default     = {}
 }
 
 variable "vpc_cidr" {
@@ -71,3 +50,8 @@ variable "dev_web_ingress_cidrs" {
   type        = list(string)
 }
 
+variable "default_tags" {
+  description = "A map of default tags to apply to all resources created by this module."
+  type        = map(string)
+  default     = {}
+}
